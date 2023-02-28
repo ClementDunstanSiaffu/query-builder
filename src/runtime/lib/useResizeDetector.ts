0680e17a405cb6f9ceba870b7627892bb3a4f8b0
pwamly <pwamly2016@gmail.com>
2023-02-28 17:24:57 +0300

    fixing default ui view

---
+++ src/runtime/lib/useResizeDetector.ts
@@ -0,0 +1,83 @@
+import { useLayoutEffect, useEffect, useState, useRef } from 'react';
+import type { MutableRefObject } from 'react';
+import type { DebouncedFunc } from 'lodash';
+
+import { patchResizeHandler, createNotifier, isSSR } from './utils';
+
+import type { PatchedResizeObserverCallback } from './utils';
+import type { Props, ReactResizeDetectorDimensions } from './ResizeDetector';
+
+const useEnhancedEffect = isSSR() ? useEffect : useLayoutEffect;
+
+export interface useResizeDetectorProps<T extends HTMLElement> extends Props {
+  targetRef?: MutableRefObject<T | null>;
+}
+
+function useResizeDetector<T extends HTMLElement = any>(
+  props: useResizeDetectorProps<T> = {}
+): UseResizeDetectorReturn<T> {
+  const {
+    skipOnMount = false,
+    refreshMode,
+    refreshRate = 1000,
+    refreshOptions,
+    handleWidth = true,
+    handleHeight = true,
+    targetRef,
+    observerOptions,
+    onResize
+  } = props;
+
+  const skipResize = useRef<boolean>(skipOnMount);
+  const localRef = useRef<T | null>(null);
+  const resizeHandler = useRef<PatchedResizeObserverCallback>();
+
+  const ref = targetRef ?? localRef;
+
+  const [size, setSize] = useState<ReactResizeDetectorDimensions>({
+    width: undefined,
+    height: undefined
+  });
+
+  useEnhancedEffect(() => {
+    if (!handleWidth && !handleHeight) return;
+
+    const notifyResize = createNotifier(onResize, setSize, handleWidth, handleHeight);
+
+    const resizeCallback: ResizeObserverCallback = (entries: ResizeObserverEntry[]) => {
+      if (!handleWidth && !handleHeight) return;
+
+      entries.forEach(entry => {
+        const { width, height } = (entry && entry.contentRect) || {};
+
+        const shouldSetSize = !skipResize.current;
+        if (shouldSetSize) {
+          notifyResize({ width, height });
+        }
+
+        skipResize.current = false;
+      });
+    };
+
+    resizeHandler.current = patchResizeHandler(resizeCallback, refreshMode, refreshRate, refreshOptions);
+
+    const resizeObserver = new window.ResizeObserver(resizeHandler.current);
+
+    if (ref.current) {
+      resizeObserver.observe(ref.current, observerOptions);
+    }
+
+    return () => {
+      resizeObserver.disconnect();
+      (resizeHandler.current as DebouncedFunc<ResizeObserverCallback>).cancel?.();
+    };
+  }, [refreshMode, refreshRate, refreshOptions, handleWidth, handleHeight, onResize, observerOptions, ref.current]);
+
+  return { ref, ...size };
+}
+
+export default useResizeDetector;
+
+export interface UseResizeDetectorReturn<T> extends ReactResizeDetectorDimensions {
+  ref: MutableRefObject<T | null>;
+}
---
+++ src/runtime/lib/utils.ts
@@ -0,0 +1,55 @@
+import debounce from 'lodash/debounce';
+import throttle from 'lodash/throttle';
+import type { DebouncedFunc } from 'lodash';
+
+import { Props, ReactResizeDetectorDimensions } from './ResizeDetector';
+
+export type PatchedResizeObserverCallback = DebouncedFunc<ResizeObserverCallback> | ResizeObserverCallback;
+
+export const patchResizeHandler = (
+  resizeCallback: ResizeObserverCallback,
+  refreshMode: Props['refreshMode'],
+  refreshRate: Props['refreshRate'],
+  refreshOptions: Props['refreshOptions']
+): PatchedResizeObserverCallback => {
+  switch (refreshMode) {
+    case 'debounce':
+      return debounce(resizeCallback, refreshRate, refreshOptions);
+    case 'throttle':
+      return throttle(resizeCallback, refreshRate, refreshOptions);
+    default:
+      return resizeCallback;
+  }
+};
+
+export const isFunction = (fn: unknown): boolean => typeof fn === 'function';
+
+export const isSSR = (): boolean => typeof window === 'undefined';
+
+export const isDOMElement = (element: unknown): boolean =>
+  element instanceof Element || element instanceof HTMLDocument;
+
+export const createNotifier =
+  (
+    onResize: Props['onResize'],
+    setSize: React.Dispatch<React.SetStateAction<ReactResizeDetectorDimensions>>,
+    handleWidth: boolean,
+    handleHeight: boolean
+  ) =>
+  ({ width, height }: ReactResizeDetectorDimensions): void => {
+    setSize(prev => {
+      if (prev.width === width && prev.height === height) {
+        // skip if dimensions haven't changed
+        return prev;
+      }
+
+      if ((prev.width === width && !handleHeight) || (prev.height === height && !handleWidth)) {
+        // process `handleHeight/handleWidth` props
+        return prev;
+      }
+
+      onResize?.(width, height);
+
+      return { width, height };
+    });
+  };
